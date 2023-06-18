6cff05f74781cfbaf0b7b7799f360824004537ef
Jallouli-Yassine <jungloul1@gmail.com>
2023-06-18 06:25:21 -0800

    lock app in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -11,6 +12,12 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  /*
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) {
+    */
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -62,4 +69,5 @@ void main() {
       home: const Expenses(), //
     ),
   );
+  // });
 }
